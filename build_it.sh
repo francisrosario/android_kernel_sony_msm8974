34b4535d1ac51e20a61ddddabb488f31d8acbe87
Francis <trinityhaxxorx@gmail.com>
2014-08-22 17:41:44 +0300

    Updated Build Script.

--- build_it.sh
+++ build_it.sh
@@ -14,17 +14,11 @@ if [ -f arch/arm/boot/zImage ]
 then
 
 echo "generating device tree..."
-./dtbTool -o ../final_files/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
+./dtbTool -o ../BuildTools/msm8974_rhine/dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
 
-cp arch/arm/boot/zImage ../final_files/.
-cp drivers/char/adsprpc.ko ../final_files/.
-cp drivers/exfat/exfat_fs.ko ../final_files/.
-cp drivers/exfat/exfat_core.ko ../final_files/.
-cp drivers/staging/prima/wlan.ko ../final_files/pronto_wlan.ko
-~/ToolChain/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded ../final_files/pronto_wlan.ko
-~/ToolChain/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded ../final_files/exfat_fs.ko
-~/ToolChain/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded ../final_files/exfat_core.ko
-~/ToolChain/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded ../final_files/nls_utf8.ko
+cp arch/arm/boot/zImage ../BuildTools/msm8974_rhine/.
+cd ~/BuildTools/msm8974_rhine
+./makeit.sh
 
 echo "DONE"
 
