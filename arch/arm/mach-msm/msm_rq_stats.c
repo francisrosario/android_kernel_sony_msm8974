64348b4e0936f4c5fe1dccdcebb006f777927e0f
Francis <trinityhaxxorx@gmail.com>
2014-10-03 16:07:47 +0300

    Revert "msm_rq_stats: squash commit to latest version"
    This reverts commit 1c48d6a40994801f9fe7ed17d6e7b56eb8834362.

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -39,7 +39,6 @@
 
 struct notifier_block freq_transition;
 struct notifier_block cpu_hotplug;
-struct notifier_block freq_policy;
 
 struct cpu_load_data {
 	cputime64_t prev_cpu_idle;
@@ -56,47 +55,42 @@ struct cpu_load_data {
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
-unsigned int get_rq_info(void)
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
 {
-	unsigned long flags = 0;
-        unsigned int rq = 0;
+	u64 idle_time;
+	u64 cur_wall_time;
+	u64 busy_time;
 
-        spin_lock_irqsave(&rq_lock, flags);
+	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
 
-        rq = rq_info.rq_avg;
-        rq_info.rq_avg = 0;
+	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
 
-        spin_unlock_irqrestore(&rq_lock, flags);
+	idle_time = cur_wall_time - busy_time;
+	if (wall)
+		*wall = jiffies_to_usecs(cur_wall_time);
 
-        return rq;
-}
-EXPORT_SYMBOL(get_rq_info);
-
-static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
-              cputime64_t *wall)
-{
-  	u64 idle_time;
-  	u64 cur_wall_time;
-  	u64 busy_time;
-
-  	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-
-  	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-  	idle_time = cur_wall_time - busy_time;
-  	
-  	if (wall)
-    	*wall = jiffies_to_usecs(cur_wall_time);
-
-  	return jiffies_to_usecs(idle_time);
+	return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wall)
+static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
+{
+	u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
+
+	if (idle_time == -1ULL)
+		return get_cpu_idle_time_jiffy(cpu, wall);
+	else
+		idle_time += get_cpu_iowait_time_us(cpu, wall);
+
+	return idle_time;
+}
+
+static inline cputime64_t get_cpu_iowait_time(unsigned int cpu,
+							cputime64_t *wall)
 {
 	u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
 
@@ -104,18 +98,6 @@ static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wal
 		return 0;
 
 	return iowait_time;
-}
-
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
-              cputime64_t *wall)
-{
-  	u64 idle_time = get_cpu_idle_time_us(cpu, wall) - 
-  						get_cpu_iowait_time(cpu, wall);
-
-  	if (idle_time == -1ULL)
-    	idle_time = get_cpu_idle_time_jiffy(cpu, wall);
-
-  	return idle_time;
 }
 
 static int update_average_load(unsigned int freq, unsigned int cpu)
@@ -172,17 +154,18 @@ static int update_average_load(unsigned int freq, unsigned int cpu)
 
 static unsigned int report_load_at_max_freq(void)
 {
-	int cpu = 0;
+	int cpu;
 	struct cpu_load_data *pcpu;
 	unsigned int total_load = 0;
 
-	pcpu = &per_cpu(cpuload, cpu);
-	mutex_lock(&pcpu->cpu_load_mutex);
-	update_average_load(pcpu->cur_freq, cpu);
-	total_load = pcpu->avg_load_maxfreq;
-	pcpu->avg_load_maxfreq = 0;
-	mutex_unlock(&pcpu->cpu_load_mutex);
-
+	for_each_online_cpu(cpu) {
+		pcpu = &per_cpu(cpuload, cpu);
+		mutex_lock(&pcpu->cpu_load_mutex);
+		update_average_load(pcpu->cur_freq, cpu);
+		total_load += pcpu->avg_load_maxfreq;
+		pcpu->avg_load_maxfreq = 0;
+		mutex_unlock(&pcpu->cpu_load_mutex);
+	}
 	return total_load;
 }
 
@@ -198,6 +181,7 @@ static int cpufreq_transition_handler(struct notifier_block *nb,
 		for_each_cpu(j, this_cpu->related_cpus) {
 			struct cpu_load_data *pcpu = &per_cpu(cpuload, j);
 			mutex_lock(&pcpu->cpu_load_mutex);
+			update_average_load(freqs->old, freqs->cpu);
 			pcpu->cur_freq = freqs->new;
 			mutex_unlock(&pcpu->cpu_load_mutex);
 		}
@@ -229,7 +213,9 @@ static int system_suspend_handler(struct notifier_block *nb,
 	switch (val) {
 	case PM_POST_HIBERNATION:
 	case PM_POST_SUSPEND:
+	case PM_POST_RESTORE:
 		rq_info.hotplug_disabled = 0;
+		break;
 	case PM_HIBERNATION_PREPARE:
 	case PM_SUSPEND_PREPARE:
 		rq_info.hotplug_disabled = 1;
@@ -240,22 +226,6 @@ static int system_suspend_handler(struct notifier_block *nb,
 	return NOTIFY_OK;
 }
 
-static int freq_policy_handler(struct notifier_block *nb,
-			unsigned long event, void *data)
-{
-	struct cpufreq_policy *policy = data;
-	struct cpu_load_data *this_cpu = &per_cpu(cpuload, policy->cpu);
-
-	if (event != CPUFREQ_NOTIFY)
-		goto out;
-
-	this_cpu->policy_max = policy->max;
-
-	pr_debug("Policy max changed from %u to %u, event %lu\n",
-			this_cpu->policy_max, policy->max, event);
-out:
-	return NOTIFY_DONE;
-}
 
 static ssize_t hotplug_disable_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
@@ -437,12 +407,9 @@ static int __init msm_rq_stats_init(void)
 	}
 	freq_transition.notifier_call = cpufreq_transition_handler;
 	cpu_hotplug.notifier_call = cpu_hotplug_handler;
-	freq_policy.notifier_call = freq_policy_handler;
 	cpufreq_register_notifier(&freq_transition,
 					CPUFREQ_TRANSITION_NOTIFIER);
 	register_hotcpu_notifier(&cpu_hotplug);
-	cpufreq_register_notifier(&freq_policy,
-					CPUFREQ_POLICY_NOTIFIER);
 
 	return ret;
 }
