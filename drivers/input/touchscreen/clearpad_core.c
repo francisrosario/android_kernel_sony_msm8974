817e89ba7aca50cbe9e98268d9e862442a3874ed
Francis <trinityhaxxorx@gmail.com>
2014-10-03 13:55:49 +0300

    Clearpad : Fix

--- drivers/input/touchscreen/clearpad_core.c
+++ drivers/input/touchscreen/clearpad_core.c
@@ -698,7 +698,7 @@ static int clearpad_flip_config_get(u8 module_id, u8 rev)
 
 static struct evgen_block *clearpad_evgen_block_get(u8 module_id, u8 rev)
 {
-	return NULL;
+	return evgen_blocks;
 }
 
 static void synaptics_clearpad_set_irq(struct synaptics_clearpad *this,
